ddc74754f6ea17e2fbdbf1b61981773210c3d78e
eohsiek <eohsiek@gmail.com>
2018-07-18 00:49:36 -0600

    tablet versus phone layout

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -14,6 +15,7 @@ import com.example.android.bakingapp.Network.GetRecipeAsync;
 public class MainActivity extends AppCompatActivity  implements
         GetRecipeAsync.OnTaskCompleted {
 
+    private int columns;
     private TextView textView;
     private RecyclerView recyclerView;
     private RecipeAdapter adapter;
@@ -24,13 +26,18 @@ public class MainActivity extends AppCompatActivity  implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        columns = getResources().getInteger(R.integer.columns);
 
-        layoutManager = new LinearLayoutManager(this);
+
+        layoutManager = new GridLayoutManager(this, columns);
         recyclerView.setLayoutManager(layoutManager);
 
         GetRecipeAsync task = new GetRecipeAsync(MainActivity.this);
         task.execute();
+
     }
+
+
 
 
     @Override
