bceb71cacef258992555744217c047fde37bba91
eohsiek <eohsiek@gmail.com>
2018-07-23 17:12:59 -0600

    two pane layout started

--- app/src/main/java/com/example/android/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/example/android/bakingapp/RecipeActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.bakingapp;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.android.bakingapp.Data.Steps;
 
 
 public class RecipeActivity extends AppCompatActivity {
@@ -16,8 +19,23 @@ public class RecipeActivity extends AppCompatActivity {
         recipeFragment.setArguments(bundle);
 
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.recipe_container, recipeFragment)
-                .commit();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.recipe_container, recipeFragment);
+
+        // This container will only initially exist in the two-pane tablet case
+        if(findViewById(R.id.steps_container) != null) {
+
+            Steps[] steps = bundle.getParcelableArrayList("steps").toArray(new Steps[0]);
+            Steps firststep = steps[0];
+            Bundle bundlesteps = new Bundle();
+            bundlesteps.putParcelable("step", firststep);
+
+            StepsFragment stepsFragment = new StepsFragment();
+            stepsFragment.setArguments(bundlesteps);
+
+            transaction.add(R.id.steps_container, stepsFragment);
+        }
+
+        transaction.commit();
     }
 }
