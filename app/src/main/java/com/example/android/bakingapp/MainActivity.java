099a1e0a44e4e200c0e51e70ef2803eedc7fd03c
eohsiek <eohsiek@gmail.com>
2018-07-24 07:43:25 -0600

    deal with offline

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.bakingapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.bakingapp.Data.Ingredients;
@@ -25,17 +29,31 @@ public class MainActivity extends AppCompatActivity  implements
     private RecyclerView recyclerView;
     private RecipeAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private Context context;
+    private TextView errorMessage;
+    private TextView recipeHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         columns = getResources().getInteger(R.integer.columns);
+        errorMessage =  findViewById(R.id.networkerror);
+        recipeHeader = findViewById(R.id.selectrecipe);
 
-        GetRecipeAsync task = new GetRecipeAsync(MainActivity.this);
-        task.execute();
+        if(isOnline()) {
+          showRecipes();
+          GetRecipeAsync task = new GetRecipeAsync(MainActivity.this);
+          task.execute();
+
+      }
+      else {
+          showError();
+      }
     }
+
 
     @Override
     public void onTaskCompleted(Recipe[] response) {
@@ -73,5 +91,25 @@ public class MainActivity extends AppCompatActivity  implements
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private  void showRecipes() {
+        errorMessage.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+        recipeHeader.setVisibility(View.VISIBLE);
+    }
+
+    private  void showError() {
+        errorMessage.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
+        recipeHeader.setVisibility(View.INVISIBLE);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo network = connectivityManager.getActiveNetworkInfo();
+        return network != null && network.isConnectedOrConnecting();
     }
 }
